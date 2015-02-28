606c461d59a865e47ee2893e8a8299acfc54521a
Dave Santos <info@davesantos.com>
2015-02-28 10:06:23 -0800

    Added fitvid.js

--- js/all.js
+++ js/all.js
@@ -1,3 +1,15 @@
 window.lazySizesConfig = {
   addClasses: true
 };
+
+$(function(){
+
+  // Target your .container, .wrapper, .post, etc.
+  var vp = $(".video-player"),
+      ap = $(".audio-player");
+
+  vp.fitVids();
+  ap.fitVids();
+
+
+});
