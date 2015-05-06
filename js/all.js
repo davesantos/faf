9c14573afad35c50a562dbbeebc7138fe43a2e1d
Dave Santos <info@davesantos.com>
2015-05-06 00:30:03 -0700

    Removed header, gray, removed icons, added lazyload fade

--- js/all.js
+++ js/all.js
@@ -2,11 +2,18 @@ window.lazySizesConfig = {
   addClasses: true
 };
 
+
+
 $(function(){
+
   // Target your .container, .wrapper, .post, etc.
   var vp = $(".video-player"),
       ap = $(".audio-player");
 
+
+
   vp.fitVids();
   ap.fitVids();
 });
+
+$(window).lazyLoadXT();
